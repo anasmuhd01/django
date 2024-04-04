b57f82e63a6421c21ae9c94bf99d13ea0d7f8b27
anas <ansmuhd01@gmail.com>
2024-04-05 05:45:33 +1200

    csrf token

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -2,11 +2,48 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
+    if request.POST:
+        print(request.POST)
     return render(request,'create.html')
 
 
 def list(request):
-    return render(request,'list.html')
+    sample_dic = {'movies':[{
+        
+        'description':'this is sample',
+        'year':'2001',
+        'success':False,
+        'img':'one.jpg'
+        },
+        {
+        'title':'two',
+        'description':'this is sample',
+        'year':'2001',
+        'success':False,
+        'img':'two.jpg'
+        },
+        {
+        'title':'three',
+        'description':'this is sample',
+        'year':'2001',
+        'success':True,
+        'img':'three.jpg'
+        },
+        {
+        'title':'four',
+        'description':'this is sample',
+        'year':'2001',
+        'success':False,
+        'img':'four.jpg'
+        },
+        {
+        'title':'five',
+        'description':'this is sample',
+        'year':'2001',
+        'success':True,
+        'img':'five.jpg'
+        },]}
+    return render(request,'list.html',sample_dic)
 
 
 def edit(request):
